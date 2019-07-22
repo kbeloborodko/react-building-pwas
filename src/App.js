02e8dec9ebb4e08bf33b0db3010b4aba2cb8dd27
Kyrylo Biloborodko <Kyrylo_Biloborodko@epam.com>
2019-07-22 13:42:29 +0300

    Add a list of video using react hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch('https://orangevalleycaa.org/api/videos').then(
+        response => response.json()
+      );
+      setData(result);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="header">
         <h1>Videos</h1>
       </header>
+      <div className="content">
+        {data.map(video => (
+          <div key={video.id} className="block">
+            <h2 className="block__heading">{video.name}</h2>
+            <video height={200} controls src={video.video_url}></video>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
